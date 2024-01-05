104a823a9df93dab3d51713d5bb1f89989ebf4d8
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-05 22:44:04 +0100

    updates MicroserviceConfigApplication and upload MicroserviceGatewayApplication; solves https://github.com/plopezgit/Saving/issues/20

---
+++ microservice-gateway/src/main/java/com/saving/microservice/gateway/MicroserviceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.saving.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewayApplication.class, args);
+	}
+
+}
